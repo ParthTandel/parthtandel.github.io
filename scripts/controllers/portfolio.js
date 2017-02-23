95708e81bb41eaf2276083665ba324a14b93edf0
parthtandel <parthtandel0594@gmail.com>
2017-02-24 00:59:58 +0530

    mobile responsive 2nd try

--- scripts/controllers/portfolio.js
+++ scripts/controllers/portfolio.js
@@ -2,9 +2,11 @@ angular.module('portfolio')
     .controller('ScrollCtrl', function($scope, $rootScope, anchorSmoothScroll) {
         $scope.activeMenu = 'Home';
         $rootScope.reverse = false;
+        $scope.toggle = false;
         $scope.gotoElement = function(eID) {
             anchorSmoothScroll.scrollTo(eID);
-            console.log($rootScope.reverse);
+            $scope.toggle = false;
+            console.log($scope.toggle);
         };
         $scope.gotoElement('home-anchor');
     });
